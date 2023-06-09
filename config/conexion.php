fe943ceef48af88836870abeddb51c490a6ede0c
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-09 15:14:20 -0500

    09-06-2023

---
+++ config/conexion.php
@@ -0,0 +1,29 @@
+<?php
+
+    session_start();
+
+    class Conectar{
+
+        protected $dbh;
+
+        protected function conexion(){
+            try{
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=masivo", "root", "");
+                return $conectar;
+            }catch(Exception $e){
+                print "Error: " .$e->getMessage() . "<br>";
+                die();
+            }
+        }
+        
+        public function set_names(){
+            return $this->conexion()->query("SET NAMES 'utf8'");
+        }
+
+        public static function ruta(){
+            return "http://localhost/EnvioMasivoCorreos_Admin/";
+        }
+    }
+
+
+?>
