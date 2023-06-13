122a68af98e1cd87fd52d13abaf9f7d98388bdc6
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-13 12:45:59 -0500

    13-06-2023

--- view/html/MainFooter.php
+++ view/html/MainFooter.php
@@ -2,11 +2,11 @@
             <footer class="footer-area">
                 <div class="row align-items-center">
                     <div class="col-lg-6 col-sm-6 col-md-6">
-                        <p>Copyright <i class='bx bx-copyright'></i> 2020 <a href="#">Fiva</a>. All rights reserved</p>
+                        <p>Copyright <i class='bx bx-copyright'></i> 2023 <a href="#">Fe y Alegría Del Perú</a>. All rights reserved</p>
                     </div>
 
                     <div class="col-lg-6 col-sm-6 col-md-6 text-right">
-                        <p>Designed by ❤️ <a href="https://envytheme.com/" target="_blank">EnvyTheme</a></p>
+                        <p>Designed by Jorge Antony <a href="https://antonymilian.com/" target="_blank">Milian Montalvo</a></p>
                     </div>
                 </div>
             </footer>
