8be3b947d76409833b4f71a94ab010878c24bce1
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-15 08:48:58 -0500

    15-06-2023

---
+++ controller/producto.php
@@ -0,0 +1,67 @@
+<?php
+
+
+require_once("../config/conexion.php");
+require_once("../models/Producto.php");
+$producto = new Producto();
+
+switch($_GET["op"]){
+
+    case "guardaryeditar":
+        if(empty($_POST["prod_id"])){
+            $producto->insert_producto($_POST["prod_nom"], $_POST["prod_precion"], $_POST["prod_preciod"], $_POST["prod_url"], $_POST["prod_img"],$_POST["prod_cupon"],$_POST["prod_descrip"]);
+        }else{
+            $producto->update_producto($_POST["prod_id"] ,$_POST["prod_nom"], $_POST["prod_precion"], $_POST["prod_preciod"], $_POST["prod_url"], $_POST["prod_img"],$_POST["prod_cupon"],$_POST["prod_descrip"]);
+        }
+        break;
+    case "listar":
+        $datos =$producto->get_producto();
+        $data = array();
+        foreach($datos as $row){
+            $sub_array = array();
+            $sub_array[] = $row["prod_nom"];
+            $sub_array[] = $row["prod_precion"];
+            $sub_array[] = $row["prod_preciod"];
+            $sub_array[] = $row["prod_cupon"];
+            $sub_array[] = '<button type="button" onClick="editar('.$row["prod_id"].')" id="'.$row["prod_id"].'" class="btn btn-outline-success"><i class="bx bx-edit"></i></button></button>';
+            $sub_array[] = '<button type="button" onClick="eliminar('.$row["prod_id"].')" id="'.$row["prod_id"].'" class="btn btn-outline-danger"><i class="bx bx-trash"></i></button></button>';
+            $data[] = $sub_array;
+        }
+
+        $results = array(
+            "sEcho" => 1,
+            "iTotalRecords" =>count($datos),
+            "iTotalDisplayRecords" =>count($datos),
+            "aaData" =>$data);
+        
+        echo json_encode($results);
+    break;
+
+    case "eliminar" :
+
+        $producto->delete_producto($_POST["prod_id"]);
+        break;
+
+
+      case "mostrar" :
+        $datos=$producto->get_producto_x_id($_POST["prod_id"]);
+
+        if(is_array($datos) == true AND count($datos)<>0){
+            foreach($datos as $row){
+                $output["prod_id"] = $row["prod_id"];
+                $output["prod_nom"] = $row["prod_nom"];
+                $output["prod_precion"] = $row["prod_precion"];
+                $output["prod_preciod"] = $row["prod_preciod"];
+                $output["prod_cupon"] = $row["prod_cupon"];
+                $output["prod_url"] = $row["prod_url"];
+                $output["prod_img"] = $row["prod_img"];
+                $output["prod_descrip"] = $row["prod_descrip"];
+            }
+        }
+        break;  
+}
+
+
+
+
+?>
--- view/mntproducto/index.php
+++ view/mntproducto/index.php
@@ -41,6 +41,35 @@
                     <li class="item">Mantenimiento de Producto</li>
                 </ol>
             </div>
+
+            <div class="card mb-30">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h3>Listado de Productos</h3>
+                    <br>
+                    <button type="button" class="btn btn-primary btn-md">Nuevo</button>
+
+                </div>
+
+
+
+                <div class="card-body">
+                <table id="producto_data" class="table display responsive nowrap">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Desc</th>
+            <th>Cupon</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody> 
+    </tbody>
+</table>
+
+            </div>
+            </div>
             <!-- End Breadcrumb Area -->
 
             <div class="flex-grow-1"></div>
