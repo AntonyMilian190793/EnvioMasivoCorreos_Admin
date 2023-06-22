440760d6793827ba461a56c27435a87da7e9a437
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-06-22 09:21:50 -0500

    22-06-2023

--- models/Email.php
+++ models/Email.php
@@ -81,17 +81,19 @@
             foreach($datos2 as $row2){
               //$this->addAddress($row2["usu_correo"]);
               $this->addBCC($row2["usu_correo"]);
+              $this->WordWrap = 50;
+              $this->IsHTML(true);
+              $this->Subject = "Fe y Alegría del Perú";
+              $cuerpo = file_get_contents('../public/Template_FyA.html');
+              $cuerpo = str_replace('$tbldetalle', $tbody, $cuerpo);
+              $cuerpo = str_replace('$xemaildesuscribirse', 'http://localhost/EnvioMasivoCorreos_Admin/view/desuscribirse/', $cuerpo);
+              $this->Body = $cuerpo;
+              $this->AltBody = strip_tags("Descuento");
             }
 
             // $this->addAddress("antonymilian19@outlook.com");
-            // $this->WordWrap = 50;
 
-            $this->IsHTML(true);
-            $this->Subject = "Fe y Alegría del Perú";
-            $cuerpo = file_get_contents('../public/Template_FyA.html');
-            $cuerpo = str_replace('$tbldetalle', $tbody, $cuerpo);
-            $this->Body = $cuerpo;
-            $this->AltBody = strip_tags("Descuento");
+
 
             try{
               $this->Send();
