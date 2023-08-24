7e6ea3386662f5ec85791a5e64ddf2af6177d0f6
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2023-08-24 12:56:29 -0500

    24-08-2023

--- view/mntproducto/index.php
+++ view/mntproducto/index.php
@@ -1,40 +1,41 @@
 <?php
 
-    require_once("../../config/conexion.php");
+require_once("../../config/conexion.php");
 
-    if(isset($_SESSION["usu_id"])){
+if (isset($_SESSION["usu_id"])) {
 
 ?>
 
-<!doctype html>
-<html lang="es">
+    <!doctype html>
+    <html lang="es">
+
     <head>
 
 
-    <?php
+        <?php
         require_once("../html/MainHead.php");
-    ?>
+        ?>
         <title>Fe y Alegría Del Perú - Correos Masivos</title>
 
         <link rel="icon" type="image/png" href="..\..\public\img\faviconfya.png">
     </head>
 
     <body>
-    <?php
+        <?php
         require_once("../html/MainMenu.php");
-    ?>
+        ?>
 
         <!-- Start Main Content Wrapper Area -->
         <div class="main-content d-flex flex-column">
 
-        <?php
-        require_once("../html/MainNavbar.php");
-    ?>
-            
+            <?php
+            require_once("../html/MainNavbar.php");
+            ?>
+
             <!-- Breadcrumb Area -->
             <div class="breadcrumb-area">
                 <h1>Mnt. Producto</h1>
-              
+
 
                 <ol class="breadcrumb">
                     <li class="item"><a href="../home/"><i class='bx bx-home-alt'></i></a></li>
@@ -53,48 +54,49 @@
 
 
                 <div class="card-body">
-                <table id="producto_data" class="table display responsive nowrap">
-    <thead>
-        <tr>
-            <th>Nro.</th>
-            <th>Nombre</th>
-            <th>Descripción</th>
-            <th></th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody> 
-    </tbody>
-</table>
+                    <table id="producto_data" class="table display responsive nowrap">
+                        <thead>
+                            <tr>
+                                <th style="width: 8%;">Nro.</th>
+                                <th>Nombre</th>
+                                <th>Descripción</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
 
-            </div>
+                </div>
             </div>
             <!-- End Breadcrumb Area -->
 
             <div class="flex-grow-1"></div>
             <?php
 
-                require_once("mntmantenimiento.php") 
+            require_once("mntmantenimiento.php")
             ?>
 
             <?php
-        require_once("../html/MainFooter.php");
-    ?>
+            require_once("../html/MainFooter.php");
+            ?>
 
         </div>
         <!-- End Main Content Wrapper Area -->
         <?php
         require_once("../html/MainJs.php");
-    ?>
-    <script tyoe="text/javascript" src="mntproducto.js"></script>
-    
+        ?>
+        <script tyoe="text/javascript" src="mntproducto.js"></script>
+
     </body>
-</html>
+
+    </html>
 
 <?php
 
-    }else{
-        header("location:" .Conectar::ruta(). "../../index.php");
-    }
+} else {
+    header("location:" . Conectar::ruta() . "../../index.php");
+}
 
 ?>
